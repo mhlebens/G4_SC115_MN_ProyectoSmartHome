fdeb949734a2a94cf86b7552a71b6bf73f940039
juanpa4275 <juanpa.arias@icloud.com>
2024-07-07 12:16:11 -0600

    Add files via upload

---
+++ Primer_avance.py
@@ -0,0 +1,14 @@
+def Menu_inicio():
+    print("Bienvenido al Home Smart")
+    nombre=input("Ingrese su nombre: ")
+    correo=input("Ingrese su correo electronico: ")
+    pin =input("Ingrese su pin: ")
+   
+    print("Su nombre de la cuenta es: ",nombre)
+    print("Su nombre de la cuenta es: ",correo)
+    print("Su pin es: ",pin)
+
+
+
+Menu_inicio()
+
