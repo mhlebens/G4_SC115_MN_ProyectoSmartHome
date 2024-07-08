c16446d08f86a8ef70c49594fc0d96b57fe16bc6
mhlebens <marifermatah@gmail.com>
2024-07-08 11:01:18 -0600

    Update Primer_avance.py

--- Primer_avance.py
+++ Primer_avance.py
@@ -1,6 +1,6 @@
 # Variables de Almacenamiento de datos
 usuarios = []
-
+casas = []
 def registro_usuarios():
     nombre=input("Ingrese su nombre: ")
     correo=input("Ingrese su correo electronico: ")
@@ -40,13 +40,45 @@ def autenticacion_usuario():
         else:
             print("PIN incorrecto. Intente de nuevo.\n")
 
-# Main del Programa
+# Función principal del programa
 def main():
     while True:
-        print("¡Bienvenido a la aplicación de Smart Home!")
+        print("¡Bienvenido a SmartHome!.")
         if not verificación_usuariosregistrados():
             registrar_usuarios()
         
         usuario = autenticacion_usuario()
         if not usuario:
             continue
+        
+        while True:
+            if not mostrar_casas(usuario):
+                registrar_casa(usuario)
+            else:
+                print("\nOpciones: \n1. Registrar nueva casa\n2. Seleccionar casa existente\n3. Cerrar sesión")
+                opcion = input("Seleccione una opción: ")
+                if opcion == '1':
+                    registrar_casa(usuario)
+                elif opcion == '2':
+                    casa_seleccionada = input("Ingrese el nombre de la casa seleccionada: ")
+                    if casa_seleccionada not in casas:
+                        print(f"No existe una casa con el nombre {casa_seleccionada}.\n")
+                        continue
+                    
+                    while True:
+                        if not mostrar_habitaciones(casa_seleccionada):
+                            registrar_habitacion(casa_seleccionada)
+                        else:
+                            print("\nOpciones: \n1. Registrar nueva habitación\n2. Volver a selección de casa\n3. Cerrar sesión")
+                            opcion = input("Seleccione una opción: ")
+                            if opcion == '1':
+                                registrar_habitacion(casa_seleccionada)
+                            elif opcion == '2':
+                                break
+                            elif opcion == '3':
+                                return
+                elif opcion == '3':
+                    break
+
+if __name__ == "__main__":
+    main()
