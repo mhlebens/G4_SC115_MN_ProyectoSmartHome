991d67c277156b6ab856c9758a91f7e0d5ca9d12
mhlebens <marifermatah@gmail.com>
2024-07-14 22:13:33 -0600

    Update Primer_avance.py
    que Dios nos acompañe...

--- Primer_avance.py
+++ Primer_avance.py
@@ -1,194 +1,154 @@
-#Registro de usuario 1 y 2 
+# Función para registrar un usuario
 def registro():
     nombre = input("Ingrese su nombre: ")
-    correo = input("Ingrese su correo electronico: ")
+    correo = input("Ingrese su correo electrónico: ")
     pin = input("Ingrese su pin: ")
-   
+    
     print("Su nombre de la cuenta es:", nombre)
-    print("Su correo electronico es:", correo)
+    print("Su correo electrónico es:", correo)
     print("Su pin es:", pin)
+    return nombre, correo, pin
 
+# Función para registrar un segundo usuario
 def segundo():
     segundo = int(input("Desea crear un segundo usuario? (1 para sí, 0 para no): "))
     if segundo == 1:
         nombre2 = input("Ingrese su nombre: ")
-        correo2 = input("Ingrese su correo electronico: ")
+        correo2 = input("Ingrese su correo electrónico: ")
         pin2 = input("Ingrese su pin: ")
 
         print("El nombre de la cuenta es:", nombre2)
-        print("El correo electronico es:", correo2)
+        print("El correo electrónico es:", correo2)
         print("El pin es:", pin2)
+        return nombre2, correo2, pin2
     else:
-        print("No se creo un segundo usuario")
-# verificacion
-def verificacion ():
-    nombre = ""
-    correo = ""
-    pin = ""
-    nombre2 = ""
-    correo2 = ""
-    pin2 = ""
-    if (nombre == "" and nombre2 ==""):
-        print ("No hay ningún usuario registrado por el momento")
-        nuevoUsuario = int(input("¿Desea registrar un nuevo usuario?\n1.Si\n2.No: "))
+        print("No se creó un segundo usuario")
+        return None, None, None
+
+# Verificación de usuarios registrados
+def verificacion(nombre, correo, pin, nombre2, correo2, pin2):
+    if nombre == "" and nombre2 == "":
+        print("No hay ningún usuario registrado por el momento")
+        nuevoUsuario = int(input("¿Desea registrar un nuevo usuario?\n1. Sí\n2. No: "))
         if nuevoUsuario == 1:
-            registro()
-            if nombre  != "":
-                print("1: ", nombre)
+            nombre, correo, pin = registro()
+            if nombre != "":
+                print("1:", nombre)
                 if nombre2 != "":
-                    print("2." + nombre)
-                seleU = int(input("Seleccione uno de los usuario:"))
+                    print("2:", nombre2)
+                seleU = int(input("Seleccione uno de los usuarios: "))
                 if seleU == 1:
-                    autenticacion1()
+                    autenticacion(pin)
                 elif seleU == 2 and nombre2 != "":
-                            autenticacion2()
+                    autenticacion(pin2)
                 else:
                     print("Selección inválida")
-        else: 
+        else:
             print("Ha salido del sistema")
     else:
-        print ("Existen usuarios registrados")
-        if nombre!= "":
-            print("1: ",nombre)
-        if nombre2!= "":
-            print("2: ",nombre2)
+        print("Existen usuarios registrados")
+        if nombre != "":
+            print("1:", nombre)
+        if nombre2 != "":
+            print("2:", nombre2)
         seleU = int(input("Seleccione uno de los usuarios: "))
         if seleU == 1:
-            autenticacion1()
-        if seleU == 2:
-            autenticacion2()
+            autenticacion(pin)
+        elif seleU == 2:
+            autenticacion(pin2)
 
-#Autenticacion por separado
-def autenticacion1 ():
+# Autenticación de usuario
+def autenticacion(pin):
     pinR = ""
-    while pinR == pin:
-        pinR= int(input("Ingrese el pin del usuario"))
+    while pinR != pin:
+        pinR = input("Ingrese el pin del usuario: ")
         if pinR != pin:
             print("Contraseña incorrecta")
-        elif pinR == pin: 
-            print ("Contraseña correcta")
-            
-def autenticacion2():
-        pinR = ""
-    while pinR == pin2:
-        pinR= int(input("Ingrese el pin del usuario"))
-        if pinR != pin2:
-            print("Contraseña incorrecta")
-        elif pinR == pin2: 
-            print ("Contraseña correcta")
-# Llamado
-registro()
-segundo()
+        else:
+            print("Contraseña correcta")
 
+# Llamado a las funciones de registro y verificación
+nombre, correo, pin = registro()
+nombre2, correo2, pin2 = segundo()
 
-#Tercera tarea(Raymundo), la idea de esta parte del proyecto es crear una proceso para agregar  casas y habitaciones( se considera que es necesario agregar, areas de la casa, como sala, dormitorio y comedor), para ello, se crean 3 variables para luego crear un ciclo, donde el usuario, al tener su casa, pueda ir creando las areas de su casa, para ello, se propone un ciclo, donde le consultara que area de la casa quiere agregar y con option de salir del sistema, para correr este proceso, primero tienen que ingresar con su usuario, pw y correo
+# Verificación de usuarios registrados
+verificacion(nombre, correo, pin, nombre2, correo2, pin2)
 
-## Ingresar a sistema previo a crear casas y registrar habitaciones
-Ingresar_a_sistema = ""
+# Registro y gestión de casas y habitaciones
+def gestionar_casas():
+    # Ingresar a sistema previo a crear casas y registrar habitaciones
+    usuario = input("Ingrese su usuario: ")
+    correo = input("Ingrese su correo: ")
+    password = input("Ingrese su contraseña: ")
+    
+    # Registrar casa
+    registrar_casa = ""
+    
+    # Ciclo para registrar casas
+    while True:
+        casaMenu = int(input("Seleccione la casa preferida\n1.Registrar Casa#1\n2.Registrar casa#2\n3.Salir\nDigite la opción que desea: "))
+        if casaMenu == 1:
+            if registrar_casa == "":
+                registrar_casa = input("Registre Casa#1: ")
+                print("Casa#1 registrada")
+        elif casaMenu == 2:
+            registrar_casa = input("Registre Casa#2: ")
+            print("Casa#2 registrada")
+        elif casaMenu == 3:
+            print("Ha salido del sistema")
+            break
+        else:
+            print("La opción seleccionada no corresponde")
 
-if Ingresar_a_sistema =="":
-        usuario = input("Ingrese su usuario:")
-        Correo = input("Ingrese su correo")
-        password = input("Ingrese su contraseña")
-        
-## Registrar casa
-## Se crean 1 variable
+    # Agregar sala, dormitorio y comedor en casa
+    agregar_sala = ""
+    agregar_dormitorio = ""
+    agregar_comedor = ""
 
-Registrar_casa = ""
+    # Ciclo para agregar áreas a la casa
+    while True:
+        opcionMenu = int(input("Seleccione la opción que desea\n1.Agregar una sala\n2.Agregar un dormitorio\n3.Agregar comedor\n4.Salir\nDigite la opción que desea: "))
+        if opcionMenu == 1:
+            if agregar_sala == "":
+                agregar_sala = input("Agregue una sala: ")
+                print("Sala agregada")
+        elif opcionMenu == 2:
+            agregar_dormitorio = input("Agregue un dormitorio: ")
+            print("Dormitorio agregado")
+        elif opcionMenu == 3:
+            agregar_comedor = input("Agregue un comedor: ")
+            print("Comedor agregado")
+        elif opcionMenu == 4:
+            print("Ha salido del sistema")
+            break
+        else:
+            print("La opción seleccionada no corresponde")
 
-##Se crea un ciclo para registrar casas
-casaMenu=1
+    # Agregar sala, dormitorio, baño y cochera en la casa
+    agregar_baño = ""
+    agregar_cochera = ""
 
-while   casaMenu !=3:
-    casaMenu = int(input("Seleccione la casa preferida\n1.Registrar Casa#1\n2.Registrar casa#2\n3.Salir\nDigite la opcion que desea:"))
-    if casaMenu ==1:
-        if Registrar_casa =="":
-            print("Registre Casa#1")
-           
-        
-    elif casaMenu ==2:
-        Registrar_casa = input("Registre casa#2")
-        print("Registre Casa#2")
+    # Ciclo para agregar más áreas a la casa
+    while True:
+        opcionMenu = int(input("Seleccione la opción que desea\n1.Agregar una sala\n2.Agregar un dormitorio\n3.Agregar baño\n4.Agregar cochera\n5.Salir\nDigite la opción que desea: "))
+        if opcionMenu == 1:
+            if agregar_sala == "":
+                agregar_sala = input("Agregue una sala: ")
+                print("Sala agregada")
+        elif opcionMenu == 2:
+            agregar_dormitorio = input("Agregue un dormitorio: ")
+            print("Dormitorio agregado")
+        elif opcionMenu == 3:
+            agregar_baño = input("Agregue un baño: ")
+            print("Baño agregado")
+        elif opcionMenu == 4:
+            agregar_cochera = input("Agregue una cochera: ")
+            print("Cochera agregada")
+        elif opcionMenu == 5:
+            print("Ha salido del sistema")
+            break
+        else:
+            print("La opción seleccionada no corresponde")
 
-                  
-    elif casaMenu ==3:
-        print("Ha salido del sistema")
-
-    else:
-        print("La opción seleccionada no corresponde")
-
-
-##Agregar sala, dormitorio y comedor en casa        
-## Se crean 3 variables
-agregar_sala = ""
-agregar_dormitorio =""
-agregar_comedor = ""
-
-##  Se trabaja en un ciclo para que el usuario elija la opcion en su casa a agregar
-opcionMenu=1
-
-while opcionMenu !=4:
-
-    opcionMenu = int(input("Seleccione la opción que desea\n1.Agregar una sala\n2.Agregar un dormitorio\n3.Agregar comedor\n4.Salir\nDigite la opción que desea:"))
-
-    if opcionMenu ==1:
-        if agregar_sala =="":
-            print("Agregue una sala")
-            
-            
-            
-    elif opcionMenu ==2:
-        agregar_dormitorio = input("Agregue un dormitorio:")
-        print("Dormitorio agregado")
-        
-    elif opcionMenu ==3:
-        agregar_comedor = input("Agregue comedor:")
-        print("Comedor agregado")
-
-    elif opcionMenu ==4:
-        print("Ha salido del sistema")
-
-    else:
-        print("La opción seleccionada no corresponde")
-## Se crean 1 variable
-
-Registrar_habitaciones = ""
-
-##Agregar sala, dormitorio , baño y cochera  en la casa
-## Se crean 3 variables
-agregar_sala = ""
-agregar_dormitorio =""
-agregar_baño = ""
-Agregar_cochera = ""
-
-##  Se trabaja en un ciclo para que el usuario elija la opcion en su casa a agregar
-opcionMenu=1
-
-while opcionMenu !=4:
-
-    opcionMenu = int(input("Seleccione la opción que desea\n1.Agregar una sala\n2.Agregar un dormitorio\n3.Agregar baño \n4. Agregar cochera \ n5.salir n Digite la opción que desea:"))
-
-    if opcionMenu ==1:
-        if agregar_sala =="":
-            print("Agregue una sala")
-            
-            
-            
-    elif opcionMenu ==2:
-        agregar_dormitorio = input("Agregue un dormitorio:")
-        print("Dormitorio agregado")
-        
-    elif opcionMenu ==3:
-        agregar_comedor = input("Agregue un baño:")
-        print("Comedor agregado")
-
-    elif opcionMenu ==4:
-       agregar_cochera = input("Agregue la cochera :")
-        print("Comedor agregado")
-
- elif opcionMenu ==5:
-        print("Ha salido del sistema")
-
-
-    else:
-        print("La opción seleccionada no disponible ")
+# Llamado a la función de gestión de casas
+gestionar_casas()
